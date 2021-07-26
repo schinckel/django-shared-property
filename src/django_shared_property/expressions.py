9a21b4564ea88acc215743fb91d03bda2e736d60
Matthew Schinckel <matt@schinckel.net>
2021-07-26 11:41:25 +0930

    Better handle complex queries.

--- src/django_shared_property/expressions.py
+++ src/django_shared_property/expressions.py
@@ -18,14 +18,14 @@ class ExpressionCol(Expression):
         return self.target.get_transform(name)
 
     def as_sql(self, compiler, connection):
-        resolved = self.target.resolved_expression
-        if getattr(resolved, "alias", None):
-            if resolved.alias not in compiler.query.alias_map:
-                compiler.query.setup_joins(
-                    self.expression.name.split("__"),
-                    self.model._meta,
-                    self.alias,
-                )
+        resolved = self.target.expression.resolve_expression(compiler.query)
+        alias = getattr(resolved, "alias", None) or self.target.model._meta.db_table
+        if alias not in compiler.query.table_map:
+            compiler.query.setup_joins(
+                self.expression.name.split("__"),
+                self.model._meta,
+                alias,
+            )
         return resolved.as_sql(compiler, connection)
 
     def get_db_converters(self, connection):
